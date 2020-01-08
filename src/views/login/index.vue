9d0b09a815f588d8f6877a1e348ecb89d8bebb82
unknown <1983883448@qq.com>
2020-01-08 21:23:40 +0800

    校验初级使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="login-contalner">
     <van-nav-bar title="登录" />
-    <van-cell-group>
-      <van-field v-model="user.mobile" clearable placeholder="请输入用户手机号" left-icon="contact" />
-
-      <van-field v-model="user.code" placeholder="请输入验证码" left-icon="contact">
-        <van-count-down v-if="isCountDownShow" slot="button" :time="1000*5" format=" ss s" @finish="isCountDownShow = false"/>
-        <van-button
-          v-else
-          slot="button"
-          size="small"
-          type="primary"
-          round
-          @click="onSendSmsCode"
-        >发送验证码</van-button>
-      </van-field>
-      <div class="login-btn-wrap">
-        <van-button type="info" @click="onLogin">登录</van-button>
-      </div>
-    </van-cell-group>
+    <ValidationObserver>
+      <ValidationProvider name="手机号" rules="required" v-slot="{errors}">
+        <van-field v-model="user.mobile" clearable placeholder="请输入用户手机号" left-icon="contact" />
+        <span>{{errors[0]}}</span>
+      </ValidationProvider>
+      <ValidationProvider>
+        <van-field v-model="user.code" placeholder="请输入验证码" left-icon="contact">
+          <van-count-down
+            v-if="isCountDownShow"
+            slot="button"
+            :time="1000*5"
+            format=" ss s"
+            @finish="isCountDownShow = false"
+          />
+          <van-button
+            v-else
+            slot="button"
+            size="small"
+            type="primary"
+            round
+            @click="onSendSmsCode"
+          >发送验证码</van-button>
+        </van-field>
+      </ValidationProvider>
+    </ValidationObserver>
+    <div class="login-btn-wrap">
+      <van-button type="info" @click="onLogin">登录</van-button>
+    </div>
   </div>
 </template>
 
